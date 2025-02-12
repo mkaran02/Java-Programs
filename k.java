281131d53b691856df029c801e1571192497def0
Karan Minchekar <karanminchekar@gmail.com>
2025-02-12 20:05:38 +0530

    checking

---
+++ k.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class k{
+    public static void main(String args[])
+    {
+
+        int a,b,c;
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter first No:");
+        a=sc.nextInt();
+        System.out.println("Enter Second No:");
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("Value of C is "+c);
+
+
+    }
+
+
+}
